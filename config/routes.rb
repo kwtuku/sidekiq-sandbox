97a47a413f0b9cc8243e70eb210e6f4570cb34c6
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-10 23:14:23 +0900

    chore: RedisとSidekiqを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'pokemons#index'
   resources :pokemons, only: %i[index]
   post 'pokemons/create_all', to: 'pokemons#create_all'
   delete 'pokemons/destroy_all', to: 'pokemons#destroy_all'
+  mount Sidekiq::Web, at: '/sidekiq'
 end
