65466df4a341e488b0c2da2e9dee04a3fbe0491c
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-11 20:55:38 +0900

    chore: 本番環境でのSidekiqの管理画面にBASIC認証を設定

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
+require 'sidekiq/web'
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+end
+
+if Rails.env.production?
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+    [user, password] == [ENV['SIDEKIQ_BASIC_AUTH_USER'], ENV['SIDEKIQ_BASIC_AUTH_PASSWORD']]
+  end
 end
